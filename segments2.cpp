1fadd9b90c6b1d3529c64278bc7e22792656df99
Ayush Bhowate <ayushpay1919@gmail.com>
2026-07-14 01:36:13 +0530

    new added

--- segments2.cpp
+++ segments2.cpp
@@ -1,17 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void mergeIntervals(vector<pair<int,int>>& vpr){
-    sort(vpr.begin() , vpr.end());
-    
-    int i = 0 ; 
-    while(i<vpr.size()){
-         int l = vpr[i].first ; 
-         int r = vpr[i].second ; 
-         
-         
+ bool isonline(int p , vector<pair<int,int>>& vpr){
+   
+    if(vpr.size() == 0)
+    return false ; 
+
+    int lo = 0 ; 
+    int hi = vpr.size()-1 ; 
+
+    while(lo<=hi){
+      int mid = (lo+hi)/2 ; 
+
+      if(p>=vpr[mid].first && p<=vpr[mid].second){
+         return true ; 
+      }
+
+      else if(p > vpr[mid].second){
+         lo = mid+1;
+      }
+      else{
+        hi = mid-1;
+      }
     }
+
+    return false ; 
+ }
+
+
+vector<pair<int,int>> mergeIntervals(vector<pair<int,int>>& vpr){
+    sort(vpr.begin() , vpr.end());
+
+    // vpr.push_back({INT_MAX , INT_MAX});
+    
+    vector<pair<int,int>> vprnew ; 
+    int tot = vpr.size();
+    int i = 0 ; 
+
+    while(i<tot){
+         int cura = vpr[i].first , curb = vpr[i].second ; 
+         while(i<tot-1 && curb >= vpr[i+1].first){
+             
+            curb = max(curb , vpr[i+1].second ) ; 
+            i++;
+         }
+
+         vprnew.push_back({cura , curb});
+         i++;
+    }
+    
+
+    return vprnew ; 
 }
+
+
 int main() {
 	// your code goes here
     
@@ -39,23 +81,58 @@ int main() {
     
      // mpx[x] would give the segments with x as common point ; 
     for(int i = 0 ; i <n-1 ; i++){
-         int xa = points[i].first ; 
-         int ya = points[i].second ; 
+         int xa = path[i].first ; 
+         int ya = path[i].second ; 
          
-         int xb = points[i+1].first ; 
-         int yb = points[i+1].second ; 
+         int xb = path[i+1].first ; 
+         int yb = path[i+1].second ; 
          
          
           if(xa == xb){
                mpx[xa].push_back({min(ya , yb) , max(ya , yb)});
           }
           else{
-              mpy[ya].push_back({min(xa , xb) , max(ya , yb)});
+              mpy[ya].push_back({min(xa , xb) , max(xa , xb)});
           }
           
     }
     
     
-  
+    for(auto& it : mpx){
+        vector<pair<int,int>>& curr = it.second ;
+         curr = mergeIntervals(curr);
+    }
+     
+   for(auto& it : mpy){
+        vector<pair<int,int>>& curr = it.second ;
+         curr = mergeIntervals(curr);
+    } 
+    
+
+    // traverse on points ; 
+     int cnt = 0 ; 
+
+    for(int i=0;i<m;i++){
+         int x = points[i].first ; 
+         int y = points[i].second ; 
+         
+         bool flag = false ; 
+
+         auto itx = mpx.find(x);
+         auto ity = mpy.find(y);
+         // on vertical line ; 
+         if(itx!=mpx.end() && isonline(y , itx->second))
+         flag = true ; 
+
+         if(!flag && ity!=mpy.end() && isonline(x , ity->second))
+         flag = true ;
+        
+         if(flag)
+         cnt++;
+
+    }
+
+    cout<<cnt<<"\n";
+
     return 0;
 }
