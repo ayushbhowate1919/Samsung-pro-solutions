838f3f26fda4c7d6810473e96e7890f85b9f53ff
Ayush Bhowate <ayush.b1@partner.samsung.com>
2026-07-13 23:58:34 +0530

    new added

--- segments2.cpp
+++ segments2.cpp
@@ -1,126 +1,61 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+void mergeIntervals(vector<pair<int,int>>& vpr){
+    sort(vpr.begin() , vpr.end());
+    
+    int i = 0 ; 
+    while(i<vpr.size()){
+         int l = vpr[i].first ; 
+         int r = vpr[i].second ; 
+         
+         
+    }
+}
 int main() {
 	// your code goes here
-	
-	 int n , m ; 
-	 cin>>n>>m ; 
-	 
-	 vector<pair<int,int>> path , points; 
-	 
-	 map<int , vector<int>> mpx, mpy ; 
-	 
-	 for(int i = 0 ; i <n ; i++){
-	      int x , y ; 
-	      cin>>x>>y ;
-	       
-	     path.push_back({x , y});
-	 }
-	  
-	  
-	 for(int i = 0 ; i <m ; i++){
-	     int x , y ; 
-	     cin>>x>>y ; 
-	     
-	     points.push_back({x , y});
-	     
-	    
-	      
-	      mpx[x].push_back(y);
-	      mpy[y].push_back(x);
-	 }
-	 
-	 for(auto& it : mpx){
-	      sort((it.second).begin() , (it.second).end());
-	 }
-	 
-	 for(auto& it : mpy){
-	      sort((it.second).begin() , (it.second).end());
-	 }
-	 
-// 	 for(auto it : mpy){
-// 	      cout<<it.first<<": ";
-	      
-// 	      for(auto itk : it.second){
-// 	           cout<<itk<<" ";
-// 	      }
-	      
-// 	      cout<<"\n";
-// 	 }	 
-	 
-	 
-	 //for every line we would check the number of points occuring on that line ; 
-	 
-	 int ans = 0 ; 
-	 for(int i = 0 ; i <n-1 ; i++){
-	      int xa = path[i].first ; 
-	      int ya = path[i].second ; 
-	      
-	      int xb = path[i+1].first ; 
-	      int yb = path[i+1].second ; 
-	      
-	   //   cout<<xa<<" "<<ya<<" "<<xb<<" "<<yb<<"\n";
-	      
-	      
-	      
-	      if(xa == xb){
-	           // we would search in [xa ,xb] range ; 
-	           
-	          vector<int>& v = mpx[xa] ;
-	          
-	          if(v.size() == 0)
-	          continue;
-	          
-	          if(ya > yb)
-	          swap(ya , yb);
-	          
-	          
-	          auto ita = lower_bound(v.begin() , v.end() , ya);
-	          auto itb = upper_bound(v.begin() , v.end() , yb);
-	          
-	       //   cout<<i<<" "<<(itb - ita)<<"\n";
-	          
-	          ans+=(itb - ita) ; 
-	          
-	          // the last point of this line would be calculated twice so we would 
-	          // remove its on occurence expect for the last line ; 
-	          if(yb == v.back() && i!=n-2){
-	              ans--;
-	          }
-	      }
-	      
-	      else{
-	         // we would search in [xa ,xb] range ; 
-	           
-	          vector<int>& v = mpy[ya] ;
-	          
-	          if(v.size() == 0)
-	          continue;
-	          
-	           if(xa > xb)
-	          swap(xa , xb);
-	          
-	          auto ita = lower_bound(v.begin() , v.end() , xa);
-	          auto itb = upper_bound(v.begin() , v.end() , xb);
-	          
-	        //  cout<<xa<<" "<<xb<<"\n";
-	       //   cout<<i<<" "<<*itb<<" "<<*ita<<"\n";
-	          
-	          ans+=(itb - ita) ; 
-	          
-	          // the last point of this line (if exist) would be calculated twice
-	          //as it would be starting point for the next line 
-	          //so we would remove its on occurence expect for the last line ; 
-	          if(xb == v.back() && i!=n-2){
-	              ans--;
-	          }          
-	          
-	      }
-	     
-	 }
-	 
-	 cout<<ans<<"\n";
-	 
-	return 0 ; 
+    
+    int n , m ; 
+    cin>>n>>m ; 
+    
+    vector<pair<int,int>> path , points;
+    
+   
+    map<int , vector<pair<int,int>>> mpx , mpy ; 
+    
+    for(int i = 0 ; i <n ;i++){
+        int x , y ; 
+        cin>>x>>y ;
+        
+        path.push_back({x , y});
+    }
+    
+    for(int i = 0 ; i <m ; i++){
+        int x , y ; 
+        cin>>x>>y ; 
+        
+        points.push_back({x ,y});
+    }
+    
+     // mpx[x] would give the segments with x as common point ; 
+    for(int i = 0 ; i <n-1 ; i++){
+         int xa = points[i].first ; 
+         int ya = points[i].second ; 
+         
+         int xb = points[i+1].first ; 
+         int yb = points[i+1].second ; 
+         
+         
+          if(xa == xb){
+               mpx[xa].push_back({min(ya , yb) , max(ya , yb)});
+          }
+          else{
+              mpy[ya].push_back({min(xa , xb) , max(ya , yb)});
+          }
+          
+    }
+    
+    
+  
+    return 0;
 }
