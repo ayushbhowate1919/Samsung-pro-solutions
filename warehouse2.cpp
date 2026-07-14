ee67f3bbe5a24c22c674fd324063faee685c5ed5
Ayush Bhowate <ayushpay1919@gmail.com>
2026-07-15 00:12:23 +0530

    new added

--- warehouse2.cpp
+++ warehouse2.cpp
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define vi vector<int>
-#define vll vector<long long>
-#define pb push_back
-#define vvi vector<vector<int>>
-#define all(x) x.begin(),x.end()
-#define print(a) for(auto it : a) {  cout<<it<<" "; }  cout<<endl; 
-#define yes()  cout << "YES" <<endl;
-#define no() std::cout << "NO" <<endl;
-/* Let's Win */
-
-void solve()
-{ 
-  int h , w , c ; 
-  cin>>h>>w>>c ; 
-  
-  vector<vector<int>> grid(n , vector<int>(m));
-
-  for(int i=0;i<h;i++){
-     for(int i=0;i<w;i++){
-          cin>>grid[i][j]; 
-     }
-  }
-
-  // this would be a dp question ; 
-
-  // dp[r][c][bitmask][d]
-  
-    
-}
-
-int main()
-{
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    int t;
-    cin >> t;
-    while(t--)
-    {
-        solve();
-    }
-    return 0;
-}
-=======
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -244,4 +197,3 @@ int main() {
     solve();
    return 0 ; 
 }
->>>>>>> 7c84a9f49b52507cfd9c70cb780377f479174317
